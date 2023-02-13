bc7ff7c8e5fe4af920d3c31949784a74fa2881cf
Guido Cardarelli <guidocarda@hotmail.com>
2023-02-13 00:15:53 -0300

    Created basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="text-container">
+        <p className="input-text"> {input} </p>
+        <p className="sample-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut minus rem
+          nobis repudiandae optio quae sint accusamus voluptate a assumenda,
+          illum ipsum iure cum sapiente? Dignissimos delectus doloremque
+          exercitationem illo?
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
