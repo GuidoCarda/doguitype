4a19b9bd5f6b8219dba4c8d4722534dafa51fee4
Guido Cardarelli <guidocarda@hotmail.com>
2023-02-20 12:08:09 -0300

    calculate gross WPM

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 const timeBounds = { cuarter: 15, half: 30, minute: 60 };
 
 const sampleText =
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque expedita explicabo animi nesciunt laboriosam blanditiis est harum? Officiis, cumque? Rerum corrupti repellat odit eaque veritatis, corporis nemo iste harum fuga hic consequuntur nam, odio magnam ad est voluptates quidem, laborum velit perferendis sequi nulla illo non vel? Voluptas assumenda porro blanditiis nulla totam nobis a expedita, dolor eos! Obcaecati ipsam eaque, voluptas in assumenda reprehenderit, quibusdam consectetur dolorum ullam eos temporibus sunt, ipsum officiis est! Illum corporis voluptatibus dicta sunt dolorem repudiandae doloremque, cumque facilis! Repudiandae, veritatis quas. Eveniet incidunt nesciunt alias voluptatem minima adipisci commodi aut placeat dolorem excepturi.";
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ab quasi distinctio. Debitis commodi, enim itaque sed optio vel, est sapiente quo porro labore tenetur cumque, hic distinctio. Quod recusandae minima facilis. Dignissimos quas officia tempore animi quos nesciunt quam eveniet vel libero expedita placeat, vitae inventore omnis vero dolor delectus qui, at facere. Deleniti delectus porro eos quibusdam molestiae quas ullam nobis labore repellendus repellat minus nihil, fugit adipisci? Doloremque doloribus vitae consectetur nesciunt. Placeat sunt ipsum voluptatem consequuntur assumenda earum accusantium veritatis architecto dicta itaque. Doloremque saepe enim recusandae iure at eos laborum, accusantium reiciendis voluptas ad aspernatur veritatis, commodi nobis soluta ducimus architecto facere dolorum sint ipsum accusamus amet exercitationem aliquam veniam! Ullam voluptatibus rerum eius aperiam voluptate deleniti dicta ipsum molestias nulla voluptates accusantium, aspernatur necessitatibus. Rem voluptatem iste eius nobis non reiciendis id molestiae quod aut, inventore saepe aliquam sapiente porro nisi sequi distinctio eveniet neque necessitatibus exercitationem dolores! Natus obcaecati reprehenderit velit recusandae voluptatibus qui fugit a maxime! Debitis assumenda delectus distinctio numquam nisi, animi nostrum fugit non rerum dicta fuga cumque, repellat asperiores exercitationem nihil impedit eaque facilis tenetur soluta quas incidunt eum? Praesentium, deleniti repellat consequatur ipsum provident id culpa ut laborum harum quia labore excepturi fugit aliquid, eligendi quasi vel voluptate voluptates. Impedit officiis beatae, molestias nobis excepturi quia atque? Tempore sit laudantium recusandae asperiores adipisci dolore hic qui tenetur aspernatur labore repellendus et, eos culpa, pariatur, quasi nihil officiis. Nobis officiis corporis ratione voluptatum placeat rem animi ipsa, dicta sunt consequatur est. Quod numquam aspernatur delectus possimus magni nobis esse, reiciendis similique explicabo illo aperiam iusto voluptatibus aliquam debitis porro corrupti animi eos accusamus modi? Assumenda repellat quasi quos. Laborum repellendus, commodi non magnam mollitia porro nostrum recusandae eveniet animi aspernatur nihil vero, nemo, repellat cum error cumque? Eveniet, facere.";
 
 function App() {
   const [input, setInput] = useState("");
@@ -12,6 +12,7 @@ function App() {
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [time, setTime] = useState(timeBounds.minute);
   const [incorrectWords, SetIncorrectWords] = useState([]);
+  const [charCount, setCharCount] = useState(0);
   const inputRef = useRef();
 
   useEffect(() => {
@@ -41,6 +42,7 @@ function App() {
 
   const handleRestart = () => {
     setCurrentWordIndex(0);
+    setCharCount(0);
     setInput("");
     setInputFocus();
     setWords(sampleText.split(" "));
@@ -59,9 +61,14 @@ function App() {
       if (!checkCharEquality(input, words[currentWordIndex])) {
         SetIncorrectWords(incorrectWords.concat(currentWordIndex));
       }
+      setCharCount((prevCount) => prevCount + input.length);
       setCurrentWordIndex((prev) => prev + 1);
       setInput("");
     }
+  };
+
+  const calculateWPM = () => {
+    return charCount / 5 / 1;
   };
 
   const checkCharEquality = (char1, char2) => char1 === char2;
@@ -74,6 +81,7 @@ function App() {
         <span>Current Input: {input}</span>
         <span>Current Word: {words[currentWordIndex]}</span>
         <span>Current Words: {words.length}</span>
+        <span>Written Characters count: {charCount}</span>
         {/* {input.length ? (
           <span>{inputMatches ? "match" : "no-match"}</span>
         ) : (
@@ -83,7 +91,7 @@ function App() {
         <span>Current word idx: {currentWordIndex}</span>
       </div>
 
-      {Boolean(time) && (
+      {Boolean(time) ? (
         <div className="text-container sample-text">
           {words.length != 0 &&
             words.map((word, idx) => (
@@ -97,6 +105,8 @@ function App() {
               />
             ))}
         </div>
+      ) : (
+        <h1 className="result">Gross WPM: {calculateWPM()}</h1>
       )}
 
       {time ? (
