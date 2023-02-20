d516ac3372342613a80286580d11167b781b2534
Guido Cardarelli <guidocarda@hotmail.com>
2023-02-19 22:01:35 -0300

    show errors letter by letter in place of showing all the word as wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+const timeBounds = { cuarter: 15, half: 30, minute: 60 };
+
 const sampleText =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima incidunt magni qui cumque rem labore facere, facilis doloribus perspiciatis ipsa vitae similique voluptatem inventore blanditiis fuga? Libero est maxime ";
+
 function App() {
   const [input, setInput] = useState("");
   const words = sampleText.split(" ");
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [time, setTime] = useState(60);
+  const [time, setTime] = useState(timeBounds.cuarter);
   const [incorrectWords, SetIncorrectWords] = useState([]);
   const inputRef = useRef();
 
@@ -80,31 +83,43 @@ function App() {
         <span>Current word idx: {currentWordIndex}</span>
       </div>
 
-      <div className="text-container sample-text">
-        {words.length != 0 &&
-          words.map((word, idx) => (
-            <Word
-              word={word}
-              key={idx}
-              input={input}
-              currentWordIndex={currentWordIndex}
-              status={incorrectWords.includes(idx) ? "incorrect" : "correct"}
-              idx={idx}
-            />
-          ))}
-      </div>
-      <div className="form">
-        <input
-          ref={inputRef}
-          type="text"
-          value={input}
-          onKeyUp={handleOnKeyUp}
-          onChange={handleInput}
-        />
-        <button type="button" onClick={handleRestart} className="btn">
-          restart
-        </button>
-      </div>
+      {Boolean(time) && (
+        <div className="text-container sample-text">
+          {words.length != 0 &&
+            words.map((word, idx) => (
+              <Word
+                word={word}
+                key={idx}
+                input={input}
+                currentWordIndex={currentWordIndex}
+                status={incorrectWords.includes(idx) ? "incorrect" : "correct"}
+                idx={idx}
+              />
+            ))}
+        </div>
+      )}
+
+      {time ? (
+        <div className="form">
+          <input
+            ref={inputRef}
+            type="text"
+            value={input}
+            onKeyUp={handleOnKeyUp}
+            onChange={handleInput}
+          />
+          <button type="button" onClick={handleRestart} className="btn">
+            restart
+          </button>
+        </div>
+      ) : (
+        <div className="finished-test">
+          <h2>Se finalizo el tiempo</h2>
+          <button type="button" onClick={handleRestart} className="btn">
+            restart
+          </button>
+        </div>
+      )}
     </div>
   );
 }
@@ -112,23 +127,75 @@ function App() {
 export default App;
 
 function Word({ word, input, currentWordIndex, idx, status }) {
+  const [errors, setErrors] = useState([]);
+
+  const isCurrentWord = currentWordIndex === idx;
+
   const matchingState = word.includes(input.trim()) ? "correct" : "incorrect";
 
+  useEffect(() => {
+    if (!isCurrentWord) return;
+    if (input[input.length - 1] === word[input.length - 1]) return;
+
+    setErrors(errors.concat(input.length - 1));
+  }, [input]);
+
   return (
-    <div
-      className={`word ${
-        currentWordIndex === idx && input.length === 0
-          ? "active"
-          : currentWordIndex === idx && input.length > 0
-          ? `active ${matchingState}`
-          : ""
-      } ${currentWordIndex > idx ? status : ""}`}
-    >
-      {[...word].map((letter, idx) => (
-        <span key={idx}>{letter}</span>
-      ))}
+    <div className={`word`}>
+      {[...word].map((letter, letterIdx) => {
+        const isCurrentLetter = input.length - 1 === letterIdx;
+
+        if (!isCurrentWord && currentWordIndex > idx) {
+          return (
+            <span
+              key={letterIdx}
+              className={`${
+                errors.includes(letterIdx) ? "incorrect" : "correct"
+              }`}
+            >
+              {letter}
+            </span>
+          );
+        }
+
+        if (isCurrentWord && input.length - 1 > letterIdx) {
+          return (
+            <span
+              key={letterIdx}
+              className={`${
+                errors.includes(letterIdx) ? "incorrect" : "correct"
+              }`}
+            >
+              {letter}
+            </span>
+          );
+        }
+
+        return (
+          <span
+            key={letterIdx}
+            className={`${
+              isCurrentWord && isCurrentLetter
+                ? input.at(-1) === letter
+                  ? "correct"
+                  : "incorrect"
+                : ""
+            }`}
+          >
+            {letter}
+          </span>
+        );
+      })}
     </div>
   );
 }
+
+// ${
+//   currentWordIndex === idx && input.length === 0
+//     ? "active"
+//     : currentWordIndex === idx && input.length > 0
+//     ? `active ${matchingState}`
+//     : ""
+// } ${currentWordIndex > idx ? status : ""}
 
 // ${currentWordIndex === idx ? "active" : ""}
