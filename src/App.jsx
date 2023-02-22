75a0052901dbebfd89dc7495a71150890a677fe3
Guido Cardarelli <guidocarda@hotmail.com>
2023-02-22 20:14:23 -0300

    fixed active word showing incorrect state on empty input

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import "./App.css";
 
 const timeBounds = { cuarter: 15, half: 30, minute: 60 };
 
-const sampleText =
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ab quasi distinctio. Debitis commodi, enim itaque sed optio vel, est sapiente quo porro labore tenetur cumque, hic distinctio. Quod recusandae minima facilis. Dignissimos quas officia tempore animi quos nesciunt quam eveniet vel libero expedita placeat, vitae inventore omnis vero dolor delectus qui, at facere. Deleniti delectus porro eos quibusdam molestiae quas ullam nobis labore repellendus repellat minus nihil, fugit adipisci? Doloremque doloribus vitae consectetur nesciunt. Placeat sunt ipsum voluptatem consequuntur assumenda earum accusantium veritatis architecto dicta itaque. Doloremque saepe enim recusandae iure at eos laborum, accusantium reiciendis voluptas ad aspernatur veritatis, commodi nobis soluta ducimus architecto facere dolorum sint ipsum accusamus amet exercitationem aliquam veniam! Ullam voluptatibus rerum eius aperiam voluptate deleniti dicta ipsum molestias nulla voluptates accusantium, aspernatur necessitatibus. Rem voluptatem iste eius nobis non reiciendis id molestiae quod aut, inventore saepe aliquam sapiente porro nisi sequi distinctio eveniet neque necessitatibus exercitationem dolores! Natus obcaecati reprehenderit velit recusandae voluptatibus qui fugit a maxime! Debitis assumenda delectus distinctio numquam nisi, animi nostrum fugit non rerum dicta fuga cumque, repellat asperiores exercitationem nihil impedit eaque facilis tenetur soluta quas incidunt eum? Praesentium, deleniti repellat consequatur ipsum provident id culpa ut laborum harum quia labore excepturi fugit aliquid, eligendi quasi vel voluptate voluptates. Impedit officiis beatae, molestias nobis excepturi quia atque? Tempore sit laudantium recusandae asperiores adipisci dolore hic qui tenetur aspernatur labore repellendus et, eos culpa, pariatur, quasi nihil officiis. Nobis officiis corporis ratione voluptatum placeat rem animi ipsa, dicta sunt consequatur est. Quod numquam aspernatur delectus possimus magni nobis esse, reiciendis similique explicabo illo aperiam iusto voluptatibus aliquam debitis porro corrupti animi eos accusamus modi? Assumenda repellat quasi quos. Laborum repellendus, commodi non magnam mollitia porro nostrum recusandae eveniet animi aspernatur nihil vero, nemo, repellat cum error cumque? Eveniet, facere.";
-
 function App() {
   const [input, setInput] = useState("");
   const [words, setWords] = useState(new Map());
@@ -108,15 +105,6 @@ function App() {
     }
   };
 
-  const calculateWPM = () => {
-    return charCount / 5 / 1;
-  };
-
-  // console.log("re-render");
-
-  const checkCharEquality = (char1, char2) => char1 === char2;
-  const checkStringEquality = (str1, str2) => str1 === str2;
-
   // console.log(currentWordIndex);
   const isInputMatching =
     words.size && words.get(currentWordIndex).includes(input);
@@ -141,7 +129,11 @@ function App() {
                   <div
                     ref={currWordRef}
                     className={`word active ${
-                      isInputMatching ? "correct" : "incorrect"
+                      input.length
+                        ? isInputMatching
+                          ? "correct"
+                          : "incorrect"
+                        : ""
                     }`}
                     key={idx}
                     id={id}
@@ -176,7 +168,7 @@ function App() {
 
       {!Boolean(time) && timerState !== "paused" && (
         <div className="result">
-          <h2>Current WPM: {calculateWPM()}</h2>
+          <h2>Current WPM: {calculateWPM(charCount)}</h2>
         </div>
       )}
       <div className="form">
@@ -203,3 +195,8 @@ function App() {
 }
 
 export default App;
+
+const calculateWPM = (charCount) => charCount / 5 / 1;
+
+const checkCharEquality = (char1, char2) => char1 === char2;
+const checkStringEquality = (str1, str2) => str1 === str2;
