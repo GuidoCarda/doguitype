81af609dfadca245eaf7f3dc8cafce7ab8b48655
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-06 13:25:53 -0300

    Added fast animation

--- src/components/ThemePicker.jsx
+++ src/components/ThemePicker.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { THEMES } from "../constants";
 import useTheme from "../hooks/useTheme";
 import { GrPaint } from "react-icons/gr";
+import { motion, AnimatePresence } from "framer-motion";
 
 const ThemePicker = () => {
   const [show, setShow] = useState(false);
@@ -23,17 +24,25 @@ const ThemePicker = () => {
       >
         <GrPaint />
       </button>
-      {show && (
-        <div className="themes-container">
-          {THEMES.map((theme) => (
-            <button
-              key={theme}
-              aria-label={`select ${theme}`}
-              onClick={() => handleThemeSelection(theme)}
-            ></button>
-          ))}
-        </div>
-      )}
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.25 }}
+            className="themes-container"
+          >
+            {THEMES.map((theme) => (
+              <button
+                key={theme}
+                aria-label={`select ${theme}`}
+                onClick={() => handleThemeSelection(theme)}
+              ></button>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
