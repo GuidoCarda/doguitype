46a2f60ee03fe9dc5fa3944fa6990afd4dc3a25c
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-06 10:57:26 -0300

    Quick Themes Refactor

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext(null);
 
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("theme-1");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("color-scheme", theme);
+  }, [theme]);
 
   const handleThemeChange = (newTheme) => {
     if (newTheme === theme) return;
