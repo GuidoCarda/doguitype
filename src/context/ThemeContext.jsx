1694665b67a57ce832bbfe79a48831084dee0384
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-06 11:31:45 -0300

    useLocalStorage custom hook, theme userPreference
    - Implemented a useLocalStorage hook to save the user theme preference
    
    - Show theme based on user preference if any

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,9 +1,18 @@
 import { createContext, useEffect, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const ThemeContext = createContext(null);
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("theme-1");
+  const [userPreference, setUserPreference] = useLocalStorage(
+    "theme",
+    "theme-1"
+  );
+  const [theme, setTheme] = useState(userPreference);
+
+  useEffect(() => {
+    setTheme(userPreference);
+  }, []);
 
   useEffect(() => {
     document.documentElement.setAttribute("color-scheme", theme);
@@ -12,6 +21,7 @@ const ThemeProvider = ({ children }) => {
   const handleThemeChange = (newTheme) => {
     if (newTheme === theme) return;
     setTheme(newTheme);
+    setUserPreference(newTheme);
   };
 
   return (
