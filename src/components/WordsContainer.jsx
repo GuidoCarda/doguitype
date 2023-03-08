f2220d5c9f8d1410add9cabaa78e9116788290fb
Guido Cardarelli <guidocarda@hotmail.com>
2023-03-07 21:06:14 -0300

    framer variants

--- src/components/WordsContainer.jsx
+++ src/components/WordsContainer.jsx
@@ -3,6 +3,11 @@ import { motion } from "framer-motion";
 import useWords from "../hooks/useWords";
 import Loader from "./Loader";
 
+const wordsContainer = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
 const WordsContainer = ({
   currentWordIndex,
   incorrectWords,
@@ -21,8 +26,9 @@ const WordsContainer = ({
         <Loader />
       ) : (
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
+          initial="hidden"
+          animate="visible"
+          variants={wordsContainer}
           className="words"
         >
           {words.size !== 0 &&
